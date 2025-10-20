33529fb15c8f6c282b07662dc537275acc1d50c9
ethandparkhurst <ethandparkhurst@gmail.com>
2025-10-20 14:12:55 -0400

    Update SMPS Combine QA Average.py

--- SMPS/SMPS Combine QA Average.py
+++ SMPS/SMPS Combine QA Average.py
@@ -1,6 +1,6 @@
-#Aydan Gibbs
-#8/28/25
-#Compiles SMPS AIM files in a designated folder and Outputs them into one csv file with no AIM meta data
+#Ethan Parkhurst and Aydan Gibbs
+#10/20/25
+#Compiles SMPS AIM files in a designated folder and Outputs them into 2 csv files, one with no AIM meta data and one containing the AIM meta data
 #Quality assures data and outputs one csv file with no AIM meta data
 #Averages data over various time steps and outputs one csv file with no AIM meta data
 
@@ -98,7 +98,10 @@ def _get_linecount(fpath, keyword, delimiter=',', encoding='ISO-8859-1'):
                 break
 
             linecount += 1
-
+            if linecount > 53:
+                linecount = 0
+                break
+    print(linecount)
     return linecount
 
 def CombineFiles():
@@ -112,6 +115,7 @@ def CombineFiles():
 
     #create a dataframe to store combined data
     dataTotal = pd.DataFrame()
+    metaTotal = pd.DataFrame()
 
     #Get the path to the data folder
     folderpath = Path(input("\nInput the full path of the folder youd like to access:\n"))
@@ -120,7 +124,8 @@ def CombineFiles():
     #itterates through each file in the user specified folder and appends them to dataTotal
     for entry in folderpath.iterdir():                                          #looks at each item in the folder
         print(entry)                                                            #prints each file name
-        metaDataLines = _get_linecount(entry, keyword = 'Scan Number')          #returns the linecount of metadata
+
+        metaDataLines = _get_linecount(entry, keyword = 'Scan Number' or 'DateTime Sample Start')          #returns the linecount of metadata
 
         meta = pd.read_table(                                                   #reads in the metadata into a df 'meta'
                         entry, 
@@ -131,27 +136,33 @@ def CombineFiles():
                         on_bad_lines='warn',
                         index_col = 0
                     ).T.iloc[0,:].to_dict()
-
         dataRaw = pd.read_table(                                                #reads in the data, skipping over the metaDataLines
                         entry,
                         skiprows=metaDataLines,
                         delimiter = ','
                     )
         
+        
+        
         dataTotal = dataTotal._append(dataRaw, ignore_index = True)             #append each file to dataTotal
+        metaTotal = metaTotal._append(meta, ignore_index = True)                #appends each metadata to metaTotal 
 
     #Convert the "DateTime Sample Start" column to a datetime object
     dataTotal["DateTime Sample Start"] = pd.to_datetime(dataTotal["DateTime Sample Start"], format = 'mixed', dayfirst=True)
     dataTotal = dataTotal.set_index('DateTime Sample Start')                    #now use the datetime object as the new index, this sorts the data by date
     dataTotal = dataTotal.sort_index()
 
-    #saves user data apon request
+    #saves user data upon request
     CreateFileYN = input('\nWould you like to save this combined file? (Y/N)\n')#promt user to save combined file
     if CreateFileYN == 'Y':                                                     #if yes, create the file at user speified location 
                                                                                 #just outside the folder the user specified earlier
-        name = input('\nEnter the desired name of your combined file and include the file type .csv:\n' \
+        name = input('\nEnter the desired name of your combined file. DO NOT INCLUDE .CSV:\n' \
                      '(This will place the file just outside the folder you indicated previously with the name you specify)\n')
-        dataTotal.to_csv(ParentPath / name)
+        final_m_name = name + 'METADATA.csv'
+        final_d_name = name + '.csv'
+        dataTotal.to_csv(ParentPath / final_d_name)
+        metaTotal.to_csv(ParentPath / final_m_name)                         #creates the csv file with name plus META with all the metadata from the combination
+
     print(dataTotal)                                                            #print the combined file for a check
     return dataTotal                                                            #return the combined file
 
