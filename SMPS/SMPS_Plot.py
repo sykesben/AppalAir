e2a61b9be76b6e3978dd4edb9c6be2eee5d8d959
ethandparkhurst <ethandparkhurst@gmail.com>
2025-11-15 19:08:51 -0500

    Update SMPS_Plot.py

--- SMPS/SMPS_Plot.py
+++ SMPS/SMPS_Plot.py
@@ -15,33 +15,18 @@ import os
 import requests
 from pathlib import Path
 
-def main():
 
-    dataTotal = pd.DataFrame()
-    dataType = "Number"
-    fileNames = []
-    csvpath = Path(input("\nInput the full path of the csv youd like to access:\n"))
-    print(csvpath)
-    df = pd.read_csv(csvpath)
-
-
-    columns_to_sum = df.iloc[:, 11 : 263 + 1]
-
-    df['sum_of_100_nm_onward'] = df[columns_to_sum].sum(axis=1)
-    print(df.head())
-
-
-def working():
+def main(): #skipping i rows for correct data range (3948 for June 2025 onward)
     import pandas as pd
 
     # Load the CSV file into a DataFrame
     file_path = 'C:\\Users\\ze_ba\\OneDrive\\Desktop\\AppalAIR\SMPS-Data\\testing\whisker\\SMPS_NumberSizeDist_2025_1hr.csv'  # Replace with your CSV file path
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path, skiprows = 3948)
 
     # Define the range of columns to sum
     # Assuming your columns are named 'col_1', 'col_2', ..., 'col_500'
-    start_col_index = 11  # Index of the first column (e.g., 'col_1')
-    end_col_index = 237  # Index of the last column (e.g., 'col_500')
+    start_col_index = 160  # Index of the first column (9 for 13.47) (121 for 100nm) (160 for 200nm)
+    end_col_index = 236  # Index of the last column (e.g., 'col_500')
 
     # Select the columns within the specified range
     # Using iloc for integer-location based indexing
@@ -50,12 +35,13 @@ def working():
     # Sum the values across the selected columns for each row
     # axis=1 specifies summing across columns (row-wise)
     df['summed_columns'] = columns_to_sum.sum(axis=1)
-
+    df['DateTime Sample Start'] = pd.to_datetime(df.iloc[:, 0])
     # Display the DataFrame with the new summed column
     print(df['summed_columns'])
-
-    # You can also save the result to a new CSV file
-    # df.to_csv('output_with_summed_columns.csv', index=False)
+    df.plot(x = 'DateTime Sample Start', y = 'summed_columns', title='Number of particles from 100nm to 800nm from June 2025 through October 2025',
+            xlabel='Time', ylabel='#',
+            color='skyblue')
+    plt.show()
 
 if __name__:
-    working()
+    main()
